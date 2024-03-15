24a939b9031368d27a3123dc4aa8f25c0195649d
Martin-Cifre <martin.cifre@outlook.com>
2024-03-15 18:22:09 -0300

    agregado formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { Header } from "./components/Navbar/navbar";
+import { Main } from "./components/Main/Main";
 
 function App() {
   return (
     <body>
       <Header />
+      <Main />
     </body>
   )
 }
