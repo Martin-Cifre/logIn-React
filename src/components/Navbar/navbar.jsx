2008f1630588167fd1b5241b0202e0a877baa88f
Martin-Cifre <martin.cifre@outlook.com>
2024-03-15 17:44:44 -0300

    headder

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { getImgUrl } from "../../utils.js";
 import styles from "./navbar.module.css";
 
 export const Header = () => {
   return (
     <section>
-      <div>Lizard</div>
+      <div className={styles.nav1}>
+        <img
+          src={getImgUrl("navbar/lizard.png")}
+          alt="Imagen Logo"
+          className={styles.navImg}
+        />
+        Lizard
+      </div>
       <ul>
         <li>log-in</li>
         <li>sign-up</li>
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const getImgUrl = (path) => {
+  return `/assets/${path}`;
+};
