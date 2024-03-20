837c455cf669da4f04b997bbc0f879fbf7d7454a
Martin-Cifre <martin.cifre@outlook.com>
2024-03-20 18:55:27 -0300

    css main

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
+/*import React from 'react'
+import { getImgUrl } from "../../utils.js";*/
+import styles from "./main.module.css";
 
 export const Main = () => {
   return (
-    <div>
-    <h2>Iniciar Sesión</h2>
-    <form action="#" method="post">
-      <input type="text" name="username" placeholder="Usuario" required />
-      <input type="password" name="password" placeholder="Contraseña" required />
-      <input type="submit" value="Iniciar Sesión" />
-    </form>
-  </div>
-  )
+    <div className={styles.container}>
+      <h2 className={styles.title}>Iniciar Sesión</h2>
+
+      <form action="#" method="post" className={styles.form}>
+        <input type="text" name="username" placeholder="Usuario" required />
+        <input
+          type="password"
+          name="password"
+          placeholder="Contraseña"
+          required
+        />
+        <input
+          type="submit"
+          value="Iniciar Sesión"
+          className={styles.button}
+        />
+      </form>
+    </div>
+  );
 }
